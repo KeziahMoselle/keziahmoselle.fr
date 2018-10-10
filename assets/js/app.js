613116c56f4dd0f9502f17abbcbced90ce3d3043
KeziahMoselle <keziahmoselle@protonmail.com>
2018-10-10 21:41:17 +0200

    Add baffle effect, Edit About, Add experience in years + Section Tools & Misc

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  M.AutoInit()
+
+  baffle('.baffle').set({ characters: '01' }).reveal(1000)
+
+  ScrollReveal({
+      reset: true
+  })
+
+  ScrollReveal().reveal('td img')
+  ScrollReveal().reveal('.card-project')
+})
