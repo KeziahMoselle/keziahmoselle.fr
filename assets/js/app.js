d49fd64a6fcf34211c35bf547b82dc6a6e5039f2
KeziahMoselle <keziahmoselle@protonmail.com>
2018-10-11 21:31:29 +0200

    Remove unecessary code, and remove reset scrollReveal

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   M.AutoInit()
 
   baffle('.baffle').set({ characters: '01' }).reveal(1000)
-
-  ScrollReveal({
-      reset: true
-  })
-
+  
   ScrollReveal().reveal('img')
   ScrollReveal().reveal('.card-project')
 })
