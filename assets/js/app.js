4569bd942dd6904f044e3721a6f16f298c302a3a
KeziahMoselle <keziah.moselle@gmail.com>
2018-11-08 23:48:49 +0100

    (WIP) Migrating to components...

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,109 @@ if (window.location.href.split('#lang=')[1]) {
   i18n.locale = window.location.href.split('#lang=')[1] || 'fr'
 }
 
+Vue.component('Skill', {
+  template: `
+    <div class="col s12 m6 l4 skills">
+      <img class="tooltipped" data-position="left" :data-tooltip="lang" :src="link" :alt="lang">
+      <div>
+        <div class="chip">{{ experience }}</div>
+      </div>
+    </div>`,
+  props: {
+    lang: String,
+    years: Number
+  },
+  computed: {
+    link: function () {
+      const parsedLink = this.lang.toLowerCase().replace(' ', '')
+      return `./assets/icons/tech/${parsedLink}.svg`
+    },
+    experience: function () {
+      if (this.years > 0) {
+        if (this.years === 1) {
+          return `${this.years} ${this.$parent.$t('skills.year')}`
+        }
+        return `${this.years} ${this.$parent.$t('skills.years')}`
+      } else {
+        return this.$parent.$t('skills.learning')
+      }
+    }
+  }
+})
+
+Vue.component('Tool', {
+  template: `
+    <div class="col s12 m6 l4 skills">
+      <img class="tooltipped" data-position="left" :data-tooltip="lang" :src="link" :alt="lang">
+      <div>
+        <div class="chip">{{ experience }}</div>
+      </div>
+    </div>`,
+  props: {
+    lang: String,
+    years: Number
+  },
+  computed: {
+    link: function () {
+      const parsedLink = this.lang.toLowerCase().replace(' ', '')
+      return `./assets/icons/tech/${parsedLink}.svg`
+    },
+    experience: function () {
+      if (this.years > 0) {
+        if (this.years === 1) {
+          return `${this.years} ${this.$parent.$t('skills.year')}`
+        }
+        return `${this.years} ${this.$parent.$t('skills.years')}`
+      } else {
+        return this.$parent.$t('skills.learning')
+      }
+    }
+  }
+})
+
+Vue.component('Project', {
+  template: `
+    <div class="col s12 m6 l4">
+      <div class="card card-project">
+        <div class="card-image">
+          <img :src="backgroundImg" :alt="title">
+          <span class="card-title">{{ title }}</span>
+          <a :href="modalId" class="modal-trigger btn-floating halfway-fab waves-effect waves-dark white"><i
+              class="material-icons">more_vert</i></a>
+        </div>
+        <div class="project card-content">
+            <span v-for="(tech, key) in technologies">
+              <img :src="tech" alt="tech">
+            </span>
+        </div>
+      </div>
+    </div>`,
+  props: {
+    title: String,
+    modalId: String,
+    tech: Array
+  },
+  computed: {
+    technologies: function () {
+      let technologies = []
+      for (i in this.tech) {
+        const parsedLink = this.filter(this.tech[i])
+        technologies.push(`./assets/icons/tech/${parsedLink}.svg`)
+      }
+      return technologies
+    },
+    backgroundImg: function () {
+      const parsedTitle = this.filter(this.title)
+      return `./assets/icons/project/${parsedTitle}.png`
+    }
+  },
+  methods: {
+    filter: function (string) {
+      return string.toLowerCase().replace(' ', '').replace('.', '').replace('.fr', '')
+    }
+  }
+})
+
 let vm = new Vue({
   el: '#app',
   i18n,
@@ -34,13 +137,10 @@ let vm = new Vue({
       if (this.isEmailValid && this.isMessageValid) {
         this.sendEmail()
       } else if (!this.isEmailValid && this.isMessageValid) {
-        console.log(messages.fr.contact.errorEmail)
         M.toast({ html: messages.fr.contact.errorEmail })
       } else if (!this.isMessageValid && this.isEmailValid) {
-        console.log(messages.fr.contact.errorMessage)
         M.toast({ html: messages.fr.contact.errorMessage })
       } else {
-        console.log(messages.fr.contact.error)
         M.toast({ html: messages[i18n.locale].contact.error })
       }
     },
