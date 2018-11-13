3e4ba688e211313c8791bb0311d0aa0fb64fe15c
KeziahMoselle <keziah.moselle@gmail.com>
2018-11-13 18:16:07 +0100

    Update translations

---
+++ src/translations/en.js
@@ -0,0 +1,36 @@
+export default {
+  header: {
+    subtitle: 'WEB ENTHUSIAST',
+    language: 'EN'
+  },
+  about: {
+    title: 'About me',
+    paragraph: 'I am a student of 18 years old and I study in the field of multimedia and internet in France.',
+    paragraph2: 'I am passionate about the web development and web design.',
+    paragraph3: 'You can see my main projects on my'
+  },
+  skills: {
+    title: 'Working with',
+    years: 'years',
+    year: 'year',
+    learning: 'Learning...'
+  },
+  tools: {
+    title: 'Tools, Frameworks and Miscellaneous'
+  },
+  portfolio: {
+    title: 'My Projects',
+    found: 'projects found.'
+  },
+  contact: {
+    title: 'Contact me',
+    emailPlaceholder: 'Your email address :',
+    messagePlaceholder: 'Your message :',
+    send: 'Send',
+    errorEmail: 'Your email is invalid.',
+    errorMessage: 'You must type a message.',
+    error: 'Please fill the two fields.',
+    sent: 'Your message has been sent !',
+    serverError: 'There was an error, please try again later.'
+  }
+}
