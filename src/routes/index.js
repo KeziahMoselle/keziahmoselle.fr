15046e808b500c8b6702451889c2a4496b7f0522
KeziahMoselle <keziah.moselle@gmail.com>
2018-11-26 22:25:00 +0100

    Change scroll behavior

--- src/routes/index.js
+++ src/routes/index.js
@@ -24,8 +24,12 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'history',
-  scrollBehavior: () => {
-    return { x: 0, y: 0 }
+  scrollBehavior: (to, from, savedPosition) => {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
   },
   routes
 })
