e8756871a84ff09eadf82b42f690fed1a5a9adfd
KeziahMoselle <keziah.moselle@gmail.com>
2019-02-12 22:49:11 +0100

    feat: circle animation in hero

--- src/Main/Hero/index.js
+++ src/Main/Hero/index.js
@@ -4,21 +4,36 @@ import './index.css'
 
 function Hero () {
   const { opacity, y } = useSpring({
-    from: { opacity: 0, y: -20 },
+    from: { opacity: 0, y: 20 },
     opacity: 1, y: 0
+  })
+  const { x, color } = useSpring({
+    from: { x: 200 },
+    x: 0, color: 'black'
   })
   
   return (
     <animated.div style={{opacity}} className="container hero">
       <div className="bar"></div>
-      <animated.h1 style={{
-        transform: y.interpolate(y => `translateY(${y}px)`)
-      }}>Keziah <br/> Moselle</animated.h1>
+
+      <animated.h1 style={{ transform: y.interpolate(y => `translateY(${y}px)`) }}>
+        Keziah <br/> Moselle
+      </animated.h1>
+
       <h2>Front-end developer.</h2>
+
       <p>
         Autodidacte de 18 ans, je suis passioné par le développement web et l'UI/UX depuis 2014.
       </p>
-      <button>Get in touch</button>
+
+      <div>
+        <button>Get in touch</button>
+        <div className="circle"></div>
+        <animated.div style={{
+          backgroundColor: color,
+          transform: x.interpolate(x => `translateX(${x}px)`)
+        }} className="circle"></animated.div>
+      </div>
     </animated.div>
   )
 }
