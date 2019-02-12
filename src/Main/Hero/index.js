5e3aaa3c4fba620a96d31fe97095961e8e0a4b9a
KeziahMoselle <keziah.moselle@gmail.com>
2019-02-12 22:06:41 +0100

    feat: animations with react-spring

--- src/Main/Hero/index.js
+++ src/Main/Hero/index.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useSpring, animated } from 'react-spring'
 import './index.css'
 
-class Hero extends Component {
-  render() {
-    return (
-      <div className="container hero">
-        <div className="bar"></div>
-        <h1>Keziah <br/> Moselle</h1>
-        <h2>Front-end developer.</h2>
-        <p>
-          Autodidacte de 18 ans, je suis passioné par le développement web et l'UI/UX depuis 2014.
-        </p>
-        <button>Get in touch</button>
-      </div>
-    )
-  }
+function Hero () {
+  const { opacity, y } = useSpring({
+    from: { opacity: 0, y: -20 },
+    opacity: 1, y: 0
+  })
+  
+  return (
+    <animated.div style={{opacity}} className="container hero">
+      <div className="bar"></div>
+      <animated.h1 style={{
+        transform: y.interpolate(y => `translateY(${y}px)`)
+      }}>Keziah <br/> Moselle</animated.h1>
+      <h2>Front-end developer.</h2>
+      <p>
+        Autodidacte de 18 ans, je suis passioné par le développement web et l'UI/UX depuis 2014.
+      </p>
+      <button>Get in touch</button>
+    </animated.div>
+  )
 }
 
 export default Hero
