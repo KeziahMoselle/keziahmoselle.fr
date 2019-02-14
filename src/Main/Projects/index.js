f31fe935d942db78206a3116b1e670caa5821244
KeziahMoselle <keziah.moselle@gmail.com>
2019-02-14 03:11:34 +0100

    feat: add github stars

--- src/Main/Projects/index.js
+++ src/Main/Projects/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
+import ky from 'ky'
 
 function Projects () {
   return (
@@ -12,7 +13,7 @@ function Projects () {
           subtitle="A simple yet featureful pomodoro in the tray/menubar"
           type="UI/UX, Frontend"
           date="2019"
-          url="/"
+          github="KeziahMoselle/pomodoro"
         />
 
         <Project
@@ -20,7 +21,7 @@ function Projects () {
           subtitle="View / Sort / Export your Starred repositories."
           type="UI/UX, Frontend, GitHub API"
           date="2018"
-          url="/"
+          github="KeziahMoselle/export-github-stars"
         />
 
         <Project
@@ -28,15 +29,25 @@ function Projects () {
           subtitle="A material client to explore an image board."
           type="UI/UX, Frontend, API"
           date="2017-2018"
-          url="/"
+          github="KeziahMoselle/gelbooru-client"
         />
       </div>
     </div>
   )
 }
 
-function Project ({ title, subtitle, type, date, url }) {
+function Project ({ title, subtitle, type, date, github }) {
+  const [stars, setStars] = useState(null)
+  /* Get the file name in /assets/thumbnails */
   const img = title.toLowerCase().replace(/\s/g, '_')
+
+  /* GitHub project */
+  if (github) {
+    (async () => {
+      const data = await ky.get(`https://api.github.com/repos/${github}`).json()
+      setStars(data.stargazers_count)
+    })()
+  }
 
   return (
     <div className="project">
@@ -44,9 +55,19 @@ function Project ({ title, subtitle, type, date, url }) {
         <div>
           <h3>{ title }</h3>
           <h4 className="grey">{ subtitle }</h4>
-          <h5>{ type } <span className="separator">⚬</span> { date }</h5>
+          <h5>
+            { type }
+            <span className="separator"></span>
+            { date }
+            { stars &&
+              <>
+                <span className="separator"></span>
+                { `${stars} ⭐` }
+              </>
+            }
+          </h5>
         </div>
-        <a href={url}><span>Read more</span></a>
+        <a href="/"><span>Read more</span></a>
       </div>
       <img src={`./assets/thumbnails/${img}.jpg`} alt={`${title} thumbnail`} />
     </div>
