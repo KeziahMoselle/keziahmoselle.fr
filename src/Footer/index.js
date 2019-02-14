7c994c3452234f00e00683c5481123a3e07b907f
KeziahMoselle <keziah.moselle@gmail.com>
2019-02-14 19:56:07 +0100

    feat: add html form and logic

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
+import ky from 'ky'
 
 function Footer () {
+  const [data, setData] = useState({
+    email: '',
+    message: ''
+  })
+
+  async function send () {
+    const response = await ky.post('/.netlify/functions/contact')
+    console.log(response)
+  }
+
   return (
     <footer>
       <div className="container">
         <h2>Contact</h2>
+        <form>
+
+          <input
+            id="email"
+            type="text"
+            value={data.email}
+            onInput={(event) => setData({ email: event.target.value })}
+          />
+
+          <textarea
+            id="message"
+            onInput={(event) => setData({ message: event.target.value })}
+          >{ data.message }</textarea>
+
+          <button onClick={send}>Submit</button>
+        </form>
       </div>
     </footer>
   )
