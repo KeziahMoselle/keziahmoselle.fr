ba81390e347a385525c2f5a1231d45ee470c9a11
KeziahMoselle <keziahmoselle@protonmail.com>
2019-02-24 01:08:48 +0100

    fix: open graph image

--- components/Seo.js
+++ components/Seo.js
@@ -18,13 +18,13 @@ function Seo () {
       <meta property="og:url" content="https://keziahmoselle.fr/" />
       <meta property="og:title" content="Keziah MOSELLE" />
       <meta property="og:description" content="Développeur front-end. Autodidacte de 18 ans, je suis passioné par le développement web et l'UI/UX depuis 2014." />
-      <meta property="og:image" content="https://keziahmoselle.fr/assets/cover.png" />
+      <meta property="og:image" content="https://keziahmoselle.fr/static/cover.png" />
       
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://keziahmoselle.fr/" />
       <meta property="twitter:title" content="Keziah MOSELLE" />
       <meta property="twitter:description" content="Développeur front-end. Autodidacte de 18 ans, je suis passioné par le développement web et l'UI/UX depuis 2014." />
-      <meta property="twitter:image" content="https://keziahmoselle.fr/assets/cover.png" />
+      <meta property="twitter:image" content="https://keziahmoselle.fr/static/cover.png" />
     </Head>
   )
 }
