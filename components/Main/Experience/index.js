c4cd831e963aac7e8ace36c4da6094bfc4b2e226
Nemesis <keziah.moselle@gmail.com>
2019-10-21 20:55:09 +0200

    feat: update experiences (#41)
    close #41

--- components/Main/Experience/index.js
+++ components/Main/Experience/index.js
@@ -1,61 +1,81 @@
 import React from 'react'
 
+const experiences = [
+  {
+    company: `vanessamoselle photographie`,
+    title: 'Développeur Full-Stack',
+    years: 'Octobre 2019',
+    url: 'https://shop.vanessamoselle.com',
+    facts: [
+      `Réalisation d'un <strong>site e-commerce</strong> à l'aide de React.js (Next.js) et Node.js`,
+      `Paiement par <strong>carte bancaire ou PayPal</strong>`
+    ]
+  },
+  {
+    company: `Piloterr (CRM/ERP)`,
+    title: 'Développeur Front-End',
+    years: '2019',
+    url: null,
+    facts: [
+      `<strong>Intégration de maquettes</strong> avec ElementUI et Vue.js`
+    ]
+  },
+  {
+    company: `Groupement d'Action Sociale de Colmar`,
+    title: 'Développeur Full-Stack',
+    years: '2018-2019',
+    url: null,
+    facts: [
+      `Refonte du site <strong>Joomla vers PHP 7</strong> (migrations base de données)`,
+      `Réalisation d'un <strong>back-office</strong>, gestion d'articles, d'utilisateurs..`,
+      `<strong>Espace membre</strong>, visualisation d'articles...`
+    ]
+  },
+  {
+    company: `Olympiades de sciences de l'ingénieur`,
+    title: `1er prix STI2D sur l'académie de Strasbourg`,
+    years: '2017-2018',
+    url: 'https://github.com/KeziahMoselle/braille-embosser',
+    facts: [
+      `Réalisation d'une <strong>application mobile</strong> avec React Native`,
+      `Développement d'un serveur Node.js sur <strong>Raspberry Pi</strong> (IoT)`
+    ]
+  },
+]
+
 function Experience () {
   return (
     <div className="container medium experience relative">
       <h3>Expérience</h3>
 
-      <div className="block card column">
-        <Chip disabled />
+      { experiences.map(experience => (
+        <div className="block card column">
+          { experience.url && <Chip link={experience.url} /> }
 
-        <h4>Groupement d'Action Sociale de Colmar</h4>
-        <h5>Développeur Full-Stack</h5>
-        <h6>2018-2019</h6>
-        <ul>
-          <li>Refonte du site <strong>Joomla vers PHP 7</strong> (migrations base de données)</li>
-          <li>Réalisation d'un <strong>back-office</strong>, gestion d'articles, d'utilisateurs..</li>
-          <li><strong>Espace membre</strong>, visualisation d'articles...</li>
-        </ul>
-      </div>
-
-      <div className="block card column">
-        <Chip link="https://github.com/KeziahMoselle/braille-embosser" />
-
-        <h4>Olympiades de sciences de l'ingénieur</h4>
-        <h5>1er prix STI2D sur l'académie de Strasbourg</h5>
-        <h6>2017-2018</h6>
-        <ul>
-          <li>Réalisation d'une application mobile avec <strong>React Native</strong></li>
-          <li>Développement d'un serveur <strong>Node.js</strong> sur Raspberry Pi</li>
-        </ul>
-      </div>
-
-      <div className="block card column">
-        <Chip link="http://justbecause.vanessamoselle.com/" />
-
-        <h4>vanessamoselle photographie</h4>
-        <h5>Développeur Front-End</h5>
-        <h6>2017</h6>
-        <ul>
-          <li>Réalisation d'une <strong>landing page</strong> pour une exposition à Los Angeles</li>
-          <li>Réalisation de 2 template emails (MJML)</li>
-        </ul>
-      </div>
+          <h4>{ experience.company }</h4>
+          <h5>{ experience.title }</h5>
+          <h6>{ experience.years }</h6>
+          <ul>
+            { experience.facts.map(fact => (
+              <li dangerouslySetInnerHTML={{ __html: fact }}></li>
+            )) }
+          </ul>
+        </div>
+      ))}
 
       <div className="circle"></div>
     </div>
   )
 }
 
-function Chip ({ link, disabled }) {
+function Chip ({ link }) {
   const href = link || '#!'
 
   return (
     <a
       className="chip"
       href={href}
-      disabled={disabled}
-      target={`${link ? '_blank' : '_self'}`}
+      target="_blank"
       rel="nofollow noopener noreferrer">
       <img src="/static/link.svg" alt="external link icon" />
     </a>
