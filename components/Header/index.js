96f5b3551be698c0180bc7b5772de152f0b93129
Nemesis <keziah.moselle@gmail.com>
2020-03-08 21:36:42 +0100

    feat: menu (#58)
    * feat: simple menu logic
    
    * feat: logo animations on scroll
    
    * feat: add menu list on scroll

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { FiMenu, FiX } from 'react-icons/fi'
 import { useTranslation } from 'react-i18next'
+import { useWindowScroll } from 'react-use'
 
 function Header () {
   const { i18n } = useTranslation()
+  const { y } = useWindowScroll()
+  const [isStickied, setIsStickied] = useState('')
+  const [isMenuExpanded, setIsMenuExpanded] = useState(false)
+
+  useEffect(() => {
+    if (y > 30) {
+      setIsStickied('stickied')
+    } else {
+      setIsStickied('')
+    }
+  }, [y])
 
   function handleTranslating () {
     if (i18n.language === 'fr') {
@@ -13,29 +26,91 @@ function Header () {
   }
 
   return (
-    <header>
-      <a href="https://keziahmoselle.fr/">
-        <img src="/static/icons/logo.svg" className="header-logo" alt="Homepage" />
-      </a>
+    <div className={`sticky-header ${isStickied}`}>
+      <header>
+        <a className="left-logo" href="https://keziahmoselle.fr/">
+          <img
+            src="/static/icons/logo.svg"
+            alt="Homepage"
+          />
+        </a>
 
-      <ul className="header-menu">
-        <li>
-          <a href="https://blog.keziahmoselle.fr/">
-            Blog
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/KeziahMoselle" rel="nofollow noopener noreferrer">
-            <img src="/static/logos/github.svg" alt="My GitHub profile" />
-          </a>
-        </li>
-        <li>
-          <a onClick={handleTranslating} href="#">
-            <img src="/static/icons/translate.svg" alt={`${i18n.language === 'fr' ? 'Translate to english' : 'Traduire en français'}`} />
-          </a>
-        </li>
-      </ul>
-    </header>
+        <ul className="appear-on-scroll">
+          <li>
+            <button onClick={handleTranslating} href="#">
+              <img
+                src="/static/icons/translate.svg"
+                alt={`${i18n.language === 'fr' ? 'Translate to english' : 'Traduire en français'}`}
+              />
+            </button>
+          </li>
+
+          <li>
+            <a
+              className="appear-logo"
+              href="https://github.com/KeziahMoselle"
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+            >
+              <img src="/static/logos/github.svg" alt="KeziahMoselle's GitHub profile" />
+            </a>
+          </li>
+
+          <li>
+            <a href="https://blog.keziahmoselle.fr/" target="_blank">
+              Blog
+            </a>
+          </li>
+        </ul>
+
+        <a className="middle-logo" href="https://keziahmoselle.fr/">
+          <img src="/static/icons/logo.svg" className="header-logo" alt="Homepage" />
+        </a>
+
+        <button
+          onClick={() => setIsMenuExpanded(!isMenuExpanded)}
+          aria-label="Menu"
+          aria-expanded={isMenuExpanded}
+          aria-controls="menu"
+        >
+          <FiMenu size={28} />
+        </button>
+        
+        <nav id="menu" className={`menu ${isMenuExpanded ? 'show' : ''}`}>
+          <div className="menu-header">
+            <button
+              onClick={() => setIsMenuExpanded(!isMenuExpanded)}
+              aria-label="Menu"
+              aria-expanded={isMenuExpanded}
+              aria-controls="menu"
+            >
+              <FiX size={28} />
+            </button>
+          </div>
+
+          <ul>
+            <li>
+              <a href="https://blog.keziahmoselle.fr/">
+                Blog
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/KeziahMoselle" rel="nofollow noopener noreferrer">
+                <img src="/static/logos/github.svg" alt="My GitHub profile" />
+              </a>
+            </li>
+            <li>
+              <a onClick={handleTranslating} href="#">
+                <img
+                  src="/static/icons/translate.svg"
+                  alt={`${i18n.language === 'fr' ? 'Translate to english' : 'Traduire en français'}`}
+                />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   )
 }
 
