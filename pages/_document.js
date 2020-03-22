6b48d8f4be41cb2286640217ebace41a8fdb4ac8
KeziahMoselle <keziah.moselle@gmail.com>
2020-03-22 22:14:05 +0100

    fix: meta document

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,10 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 
+const name = 'Keziah MOSELLE'
+const domainUrl = 'https://keziahmoselle.fr/'
+const metaDescription = "Portfolio de Keziah Moselle, développeur front-end autodidacte depuis 6 ans et passionné par le développement web et l'UI/UX depuis 2014."
+const coverImageUrl = 'https://keziahmoselle.fr/cover.png'
+
 class HTMLDocument extends Document {
   render () {
     return (
@@ -7,23 +12,23 @@ class HTMLDocument extends Document {
         <Head>
           <meta	name="viewport"	content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-          <meta name="title" content="Keziah MOSELLE" />
-          <meta name="description" content="Développeur front-end. Autodidacte de 18 ans, je suis passioné par le développement web et l'UI/UX depuis 2014." />
+          <meta name="title" content={name} />
+          <meta name="description" content={metaDescription} />
 
           <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="canonical" href="https://keziahmoselle.fr/" />
+          <link rel="canonical" href={domainUrl} />
 
           <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://keziahmoselle.fr/" />
-          <meta property="og:title" content="Keziah MOSELLE" />
-          <meta property="og:description" content="Développeur front-end. Autodidacte de 18 ans, je suis passioné par le développement web et l'UI/UX depuis 2014." />
-          <meta property="og:image" content="https://keziahmoselle.fr/cover.png" />
+          <meta property="og:url" content={domainUrl} />
+          <meta property="og:title" content={name} />
+          <meta property="og:description" content={metaDescription} />
+          <meta property="og:image" content={coverImageUrl} />
 
           <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://keziahmoselle.fr/" />
-          <meta property="twitter:title" content="Keziah MOSELLE" />
-          <meta property="twitter:description" content="Développeur front-end. Autodidacte de 18 ans, je suis passioné par le développement web et l'UI/UX depuis 2014." />
-          <meta property="twitter:image" content="https://keziahmoselle.fr/cover.png" />
+          <meta property="twitter:url" content={domainUrl} />
+          <meta property="twitter:title" content={name} />
+          <meta property="twitter:description" content={metaDescription} />
+          <meta property="twitter:image" content={coverImageUrl} />
           <noscript>
             <div className="flex center">
               <div className="card noscript">
