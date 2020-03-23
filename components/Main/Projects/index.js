35f394113d4bed4e34fb3190661546de16a00c46
KeziahMoselle <keziah.moselle@gmail.com>
2020-03-23 17:44:10 +0100

    feat: sort big projects by stars

--- components/Main/Projects/index.js
+++ components/Main/Projects/index.js
@@ -5,12 +5,16 @@ import Project from './Project'
 function Projects ({ projects }) {
   const { t } = useTranslation()
 
-  const bigProjects = projects.reduce((acc, project, index) => {
-    if (project.isBigProject) {
-      acc.push(<Project key={index} {...project} />)
-    }
-    return acc
-  }, [])
+  const bigProjects = projects
+    .reduce((acc, project, index) => {
+      if (project.isBigProject) {
+        acc.push(<Project key={index} {...project} />)
+      }
+      return acc
+    }, [])
+    .sort((a, b) => b.props.repo.user.repository.stargazers.totalCount - a.props.repo.user.repository.stargazers.totalCount)
+
+  console.log(bigProjects)
 
   const smallProjects = projects.reduce((acc, project, index) => {
     if (!project.isBigProject) {
