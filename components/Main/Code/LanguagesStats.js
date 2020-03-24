8c7096aa1f1a8c4abc49421d156c719aeb4777b8
KeziahMoselle <keziah.moselle@gmail.com>
2020-03-24 20:00:12 +0100

    feat: add label language

--- components/Main/Code/LanguagesStats.js
+++ components/Main/Code/LanguagesStats.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import filterRepositoriesBytes from '../../utils/filterRepositoriesBytes'
 
 function LanguagesStats ({ userStats }) {
   const { t } = useTranslation()
+  const [label, setLabel] = useState('')
+
   const { percentages, languagesColor } = filterRepositoriesBytes(userStats.repositories.nodes)
 
+  function handleOnMouseOver (event) {
+    const labelToShow = event.target.getAttribute('aria-label')
+    setLabel(labelToShow)
+  }
+
+  function handleOnMouseLeave () {
+    setLabel('')
+  }
+
   return (
-    <div className="block column valign">
+    <div
+      className="block column valign"
+      onMouseLeave={handleOnMouseLeave}
+    >
       <h4>{ t('code.languagesStats') }</h4>
 
       <div className="languages-stats">
@@ -15,6 +29,7 @@ function LanguagesStats ({ userStats }) {
           <React.Fragment key={language}>
             <h6>{ language }</h6>
             <span
+              onMouseOver={handleOnMouseOver}
               className="language-color"
               aria-label={`${language} ${Math.round(percentage)}%`}
               style={{
@@ -27,6 +42,7 @@ function LanguagesStats ({ userStats }) {
           </React.Fragment>
         )) }
       </div>
+      <h6 className="language-label">{ label }</h6>
     </div>
   )
 }
