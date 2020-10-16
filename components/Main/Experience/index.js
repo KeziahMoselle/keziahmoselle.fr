da493bfff25243130bd7c5f0cc3ec94c75686bdd
Nemesis <keziah.moselle@gmail.com>
2020-10-16 19:20:06 +0200

    feat: update pro experience

--- components/Main/Experience/index.js
+++ components/Main/Experience/index.js
@@ -5,54 +5,15 @@ import Planet from '../../Planet'
 
 const experiences = [
   {
-    company: 'Valanka',
-    title: 'Développeur Full-Stack',
-    years: 'Avril 2020',
-    url: 'https://valanka.fr/',
-    facts: [
-      'Réalisation d\'un blog avec <strong>Ghost CMS</strong>',
-      'Réalisation d\'un module de <strong>newsletter</strong>'
-    ]
-  },
-  {
-    company: 'vanessamoselle photographie',
-    title: 'Développeur Full-Stack',
-    years: 'Octobre 2019',
-    url: 'https://shop.vanessamoselle.com',
-    facts: [
-      'Réalisation d\'un <strong>site e-commerce</strong> à l\'aide de React.js (Next.js) et Node.js',
-      'Paiement par <strong>carte bancaire ou PayPal</strong>'
-    ]
-  },
-  {
-    company: 'Piloterr (CRM/ERP)',
+    company: 'Studio Meta',
     title: 'Développeur Front-End',
-    years: '2019',
-    url: null,
-    facts: [
-      '<strong>Intégration de maquettes</strong> avec ElementUI et Vue.js'
-    ]
+    years: 'Octobre 2020 - Aujourd\'hui',
+    url: 'https://studiometa.fr/'
   },
   {
-    company: 'Groupement d\'Action Sociale de Colmar',
-    title: 'Développeur Full-Stack',
-    years: '2018-2020',
-    url: null,
-    facts: [
-      'Refonte du site <strong>Joomla vers PHP 7</strong> (migrations base de données)',
-      'Réalisation d\'un <strong>back-office</strong>, gestion d\'articles, d\'utilisateurs..',
-      '<strong>Espace membre</strong>, visualisation d\'articles...'
-    ]
-  },
-  {
-    company: 'Olympiades de sciences de l\'ingénieur',
-    title: '1er prix STI2D sur l\'académie de Strasbourg',
-    years: '2017-2018',
-    url: 'https://github.com/KeziahMoselle/braille-embosser',
-    facts: [
-      'Réalisation d\'une <strong>application mobile</strong> avec React Native',
-      'Développement d\'un serveur Node.js sur <strong>Raspberry Pi</strong> (IoT)'
-    ]
+    company: 'Freelance',
+    title: 'Développeur web',
+    years: 'Juin 2018 - Aujourd\'hui'
   }
 ]
 
@@ -68,15 +29,16 @@ function Experience () {
           <h4>{ experience.company }</h4>
           <h5>{ experience.title }</h5>
           <h6>{ experience.years }</h6>
-          <ul>
-            { experience.facts.map((fact, index) => (
-              <li
-                key={index}
-                dangerouslySetInnerHTML={{ __html: fact }}
-              >
-              </li>
-            )) }
-          </ul>
+          { experience.facts && (<ul>
+              { experience.facts.map((fact, index) => (
+                <li
+                  key={index}
+                  dangerouslySetInnerHTML={{ __html: fact }}
+                >
+                </li>
+              )) }
+            </ul>)
+          }
           { experience.url && (
             <Chip
               link={experience.url}
